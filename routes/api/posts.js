2f17042af70bb04c7bb565f860f2487f5f818e00
Subham Jaguri <techlious1@gmail.com>
2020-02-08 02:45:12 +0530

    like and unlike post feature added

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -117,18 +117,18 @@ router.put('/like/:id', auth, async (req, res) => {
       return res.status(400).json({ msg: 'Post not found' });
     }
 
-    //check if the post has already been liked 
-    if(post.likes.filter(like => like.user.toString() === req.user.id).length > 0){
-        
+    //check if the post has already been liked
+    if (
+      post.likes.filter(like => like.user.toString() === req.user.id).length > 0
+    ) {
+      return res.status(400).json({ msg: 'Post already liked' });
     }
 
-    if (post.user.toString() !== req.user.id) {
-      return res.status(401).json({ msg: 'User not authorized' });
-    }
+    post.likes.unshift({ user: req.user.id });
 
-    await post.remove();
+    await post.save();
 
-    res.json({ msg: 'Post Removed' });
+    res.json(post.likes);
   } catch (err) {
     if (err.kind === 'ObjectId') {
       return res.status(400).json({ msg: 'Post not found' });
@@ -138,26 +138,33 @@ router.put('/like/:id', auth, async (req, res) => {
   }
 });
 
-// @route       PUT api/posts/comment/:id
-// @desc        comment on a post
+// @route       PUT api/posts/unlike/:id
+// @desc        Unlike a post
 // @access      Private
 
-router.put('/comment/:id', auth, async (req, res) => {
+router.put('/unlike/:id', auth, async (req, res) => {
   try {
     const post = await Post.findById(req.params.id);
     if (!post) {
       return res.status(400).json({ msg: 'Post not found' });
     }
 
-    //check user
-
-    if (post.user.toString() !== req.user.id) {
-      return res.status(401).json({ msg: 'User not authorized' });
+    //check if the post is not already been liked
+    if (
+      post.likes.filter(like => like.user.toString() === req.user.id).length ===
+      0
+    ) {
+      return res.status(400).json({ msg: 'Post has not yet been liked' });
     }
 
-    await post.remove();
+    //get remove index
+    const removeIndex = post.likes
+      .map(like => like.user.toString())
+      .indexOf(req.user.id);
+    post.likes.splice(removeIndex, 1);
+    await post.save();
 
-    res.json({ msg: 'Post Removed' });
+    res.json(post.likes);
   } catch (err) {
     if (err.kind === 'ObjectId') {
       return res.status(400).json({ msg: 'Post not found' });
@@ -166,4 +173,5 @@ router.put('/comment/:id', auth, async (req, res) => {
     res.status(500).send('Server Error');
   }
 });
+
 module.exports = router;
