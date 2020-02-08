f9469ada4954ed18de980ec77a4801319ca4f270
Subham Jaguri <techlious1@gmail.com>
2020-02-08 19:11:03 +0530

    D

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -174,4 +174,43 @@ router.put('/unlike/:id', auth, async (req, res) => {
   }
 });
 
+// @route       POST api/posts/comment/:id
+// @desc        create a Post
+// @access      Private
+router.post(
+  '/comment/:id',
+  [
+    auth,
+    [
+      check('text', 'Text is required')
+        .not()
+        .isEmpty()
+    ]
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+      const user = await User.findById(req.user.id).select('-password');
+      const post = await Post.findById(req.params.id);
+
+      const newPost = new Post({
+        text: req.body.text,
+        name: user.name,
+        avatar: user.avatar,
+        user: req.user.id
+      });
+
+      const post = await newPost.save();
+      res.json(post);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
+  }
+);
+
 module.exports = router;
